2a13af000762cfd39eaa52b6a824ea29a948301f
Alexander Else <aelse@else.id.au>
2018-06-10 15:15:33 +1000

    Wait to see published message before disconnecting mqtt client

--- commands.go
+++ commands.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/yosssi/gmq/mqtt"
 	"github.com/yosssi/gmq/mqtt/client"
@@ -25,17 +26,20 @@ func garage(addr, username, password string) Command {
 
 	return func(ctx context.Context, msg string) string {
 		args := strings.Split(msg, " ")
-		usage := fmt.Sprintf("Usage: %s <entry|exit>", args[0])
+		usage := fmt.Sprintf("Usage: %s <entry|exit|ping>", args[0])
 
-		if len(args) < 2 || (args[1] != "entry" && args[1] != "exit") {
+		if len(args) < 2 || (args[1] != "entry" && args[1] != "exit" && args[1] != "ping") {
 			return usage
 		}
-		topic := "garage/" + args[1]
+		gate := args[1]
+		topic := "garage/" + gate
+
+		recvErr := make(chan error)
 
 		mq := client.New(&client.Options{
 			// Define the processing of the error handler.
 			ErrorHandler: func(err error) {
-				fmt.Println(err)
+				recvErr <- err
 			},
 		})
 		defer mq.Terminate()
@@ -52,6 +56,34 @@ func garage(addr, username, password string) Command {
 		}
 		defer mq.Disconnect()
 
+		// Subscribe to topics.
+		err = mq.Subscribe(&client.SubscribeOptions{
+			SubReqs: []*client.SubReq{
+				&client.SubReq{
+					TopicFilter: []byte(topic),
+					QoS:         mqtt.QoS0,
+					// Define the processing of the message handler.
+					Handler: func(topicName, message []byte) {
+						// Saw message to our topic. Flag success by sending nil to error channel.
+						recvErr <- nil
+					},
+				},
+			},
+		})
+		if err != nil {
+			return fmt.Sprintf("Error subscribing to topic %s: %v", topic, err.Error())
+		}
+
+		// Unsubscribe from topics upon return.
+		defer func() {
+			// Ignore error because we're closing the client anyway.
+			_ = mq.Unsubscribe(&client.UnsubscribeOptions{
+				TopicFilters: [][]byte{
+					[]byte(topic),
+				},
+			})
+		}()
+
 		err = mq.Publish(&client.PublishOptions{
 			QoS:       mqtt.QoS0,
 			TopicName: []byte(topic),
@@ -62,6 +94,20 @@ func garage(addr, username, password string) Command {
 			return fmt.Sprintf("Error publishing to topic %s: %v", topic, err.Error())
 		}
 
-		return "Triggered opening the " + args[1] + " gate."
+		select {
+		// Channel returns an error when something failed or nil once a message seen on topic.
+		case err = <-recvErr:
+			if err != nil {
+				return fmt.Sprintf("Failed to trigger gate: %v", err)
+			}
+			// Timeout after a few seconds of waiting.
+		case <-time.NewTicker(5 * time.Second).C:
+			return "Timed out waiting for message on topic."
+		}
+
+		if gate == "ping" {
+			return "Pinged MQTT path successfully."
+		}
+		return "Triggered opening the " + gate + " gate."
 	}
 }
